b539ca900d8043ff9ef748a00cf6d2bf60639062
Amrithesh S <94913836+amritheshsiva@users.noreply.github.com>
2023-11-12 21:59:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,12 @@
+// script.js
 const apiKey = '95c4bc4204d2e412aa82f70c15889d1f';
 const city = 'Trivandrum';
-const country = 'Kerala';
+const country = 'India';
 
 const location = `${city},${country}`;
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${Trivandrm}&appid=${95c4bc4204d2e412aa82f70c15889d1f}`;
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
 
 function getWeather() {
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            displayWeather(data);
-        })
-        .catch(error => {
-            console.error('Error fetching weather data:', error);
-            alert('Error fetching weather data. Please try again.');
-        });
-}
-
-function displayWeather(data) {
-    const weatherContainer = document.getElementById('weather-container');
-
-    const cityName = data.name;
-    const temperature = Math.round(data.main.temp - 273.15); 
-    const weatherDescription = data.weather[0].description;
-
-    const weatherHTML = `
-        <h2>${cityName}</h2>
-        <p>Temperature: ${temperature} °C</p>
-        <p>Weather: ${weatherDescription}</p>
-    `;
-
-    weatherContainer.innerHTML = weatherHTML;
-    function getWeather() {
     console.log('Fetching weather data...');
 
     fetch(apiUrl)
@@ -49,4 +24,21 @@ function displayWeather(data) {
         });
 }
 
+function displayWeather(data) {
+    const weatherContainer = document.getElementById('weather-container');
+
+    const cityName = data.name;
+    const temperature = Math.round(data.main.temp - 273.15); // Convert from Kelvin to Celsius
+    const weatherDescription = data.weather[0].description;
+
+    const weatherHTML = `
+        <h2>${cityName}</h2>
+        <p>Temperature: ${temperature} °C</p>
+        <p>Weather: ${weatherDescription}</p>
+    `;
+
+    weatherContainer.innerHTML = weatherHTML;
 }
+
+// Fetch weather data when the script is loaded
+getWeather();
