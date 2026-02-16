566cda636501424baacdbbc4173789bedd02f8f0
Amrithesh S <amritheshsiva@gmail.com>
2026-02-16 13:47:19 +0530

    weather API URL construction

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
 const apiKey = "d36988af5c3ecc8027255d80a9597402";
-
 function getWeather() {
-
     const city = document.getElementById("city").value;
-
     if (!city) {
         document.getElementById("weatherInfo").innerHTML =
             `<div class="alert alert-warning">Please enter a city name</div>`;
         return;
     }
-
-    const url =
-        "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&appid=" + apiKey +
-        "&units=metric";
-
+    const url ="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric";
     fetch(url)
         .then(res => res.json())
         .then(data => {
-
             if (data.cod === 200) {
-
                 document.getElementById("weatherInfo").innerHTML = `
                     <h4 class="mt-3">${data.name}, ${data.sys.country}</h4>
                     <h3>${data.main.temp}°C</h3>
@@ -38,12 +27,11 @@ function getWeather() {
                         </div>
                     </div>
                 `;
-
-            } else {
+            } 
+            else {
                 document.getElementById("weatherInfo").innerHTML =
                     `<div class="alert alert-danger">City not found</div>`;
             }
-
         })
         .catch(() => {
             document.getElementById("weatherInfo").innerHTML =
